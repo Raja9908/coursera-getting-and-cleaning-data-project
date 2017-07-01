0fd44f509e675560122a3f6303a752692e2a067a
Raja9908 <Raja9908@users.noreply.github.com>
2017-07-01 14:23:08 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,11 @@
+library(plyr)
 
+
+if(!file.exists("./data")){dir.create("./data")}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+download.file(fileUrl,destfile="./data/Dataset.zip")
 
 Merging the training and the test sets to create one data set:
 
